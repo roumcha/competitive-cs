ee3b65ee743b242fa975576a9da9c9d9b3f69555
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-09-16 20:01:02 +0900

    素因数分解O(sqrt(n))版

--- my-library/Prime.cs
+++ my-library/Prime.cs
@@ -1,8 +1,9 @@
-static class Prime {
+static class PrimeNumber {
   /// <summary>素数か否か</summary>
   /// <remarks>O(√N)</remarks>
   [MI(256)]
   public static bool IsPrime(uint n) {
+    if (n == 0) throw new ArgumentOutOfRangeException(nameof(n), "input must not be 0");
     if ((n & 1) == 0 || n <= 2) return n == 2;
     for (ulong i = 3; i * i <= n; i += 2)
       if (n % i == 0) return false;
@@ -12,12 +13,12 @@ static class Prime {
   /// <summary>素数か否か一覧</summary>
   /// <remarks>O(N log log N)</remarks>
   public static bool[] Sieve(uint max) {
-    if (max <= 1) return new bool[max];
+    if (max <= 1) throw new ArgumentOutOfRangeException(nameof(max), "input must be >= 2");
     var s = new bool[(ulong)max + 1];
     Array.Fill(s, true); s[1] = false;
     for (uint i = 2; i <= max; i++) {
       if (!s[i]) continue;
-      for (var j = (ulong)i * i; j <= max; j += i) s[j] = false;
+      for (ulong j = (ulong)i * i; j <= max; j += i) s[j] = false;
     }
     return s;
   }
@@ -25,7 +26,7 @@ static class Prime {
   /// <summary>最小の素因数一覧 (Smallest prime factor)</summary>
   /// <remarks>O(N log log N)</remarks>
   public static uint[] Spfs(uint max) {
-    if (max <= 1) return new uint[0];
+    if (max <= 1 || max == uint.MaxValue) throw new ArgumentOutOfRangeException(nameof(max), "input must be >= 2 and < uint.MaxValue");
     var s = new uint[(ulong)max + 1];
     for (uint i = 2; i <= max; i++) {
       if (s[i] != 0) continue;
@@ -38,18 +39,56 @@ static class Prime {
   }
 }
 
+/// <summary>前計算あり・なしの素因数分解</summary>
 class PrimeFactorizer {
-  uint[] s;
+  readonly uint[] _spfs;
+
   /// <remarks>O(N log log N)</remarks>
-  public PrimeFactorizer(uint max) { s = Prime.Spfs(max); }
-  /// <summary>O(log N)</summary>
-  public List<(uint P, uint Cnt)> Factorize(uint n) {
-    var r = new List<(uint P, uint Cnt)>();
+  public PrimeFactorizer(uint precalcMax) {
+    if (precalcMax <= 1 || precalcMax == uint.MaxValue) throw new ArgumentOutOfRangeException(nameof(precalcMax), "input must be >= 2 and < uint.MaxValue");
+    _spfs = PrimeNumber.Spfs(precalcMax);
+  }
+
+  public PrimeFactorizer(uint[] spfs) => _spfs = spfs;
+
+  /// <remarks>O(log N)</remarks>
+  public List<(uint Prime, uint Cnt)> FactorizeFast(uint n) {
+    if (n <= 1 || n == uint.MaxValue) throw new ArgumentOutOfRangeException(nameof(n), "input must be >= 2 and < uint.MaxValue");
+    if (n == uint.MaxValue) return new() { (3, 1), (5, 1), (17, 1), (257, 1), (65537, 1) };
+    var res = new List<(uint Prime, uint Cnt)>();
+
+    // 2はビットで処理
+    int two_cnt = BitOperations.TrailingZeroCount(n);
+    n >>= two_cnt;
+    if (two_cnt > 0) res.Add((2, (uint)two_cnt));
+
+    // 3以上
     while (n > 1) {
-      uint p = s[n], e = 0;
-      while (s[n] == p) { n /= p; e++; }
-      r.Add((p, e));
+      uint prime = _spfs[n], e = 0;
+      while (_spfs[n] == prime) { n /= prime; e++; }
+      res.Add((prime, e));
     }
-    return r;
+    return res;
+  }
+
+  /// <remarks>O(sqrt(n))</remarks>
+  public static List<(ulong Prime, uint Cnt)> Factorize(ulong n) {
+    if (n <= 1) throw new ArgumentOutOfRangeException(nameof(n), "input must be >= 2");
+    var res = new List<(ulong Prime, uint Cnt)>();
+
+    // 2はビットで処理
+    int two_cnt = BitOperations.TrailingZeroCount(n);
+    n >>= two_cnt;
+    if (two_cnt > 0) res.Add((2, (uint)two_cnt));
+
+    // 3以上
+    for (ulong i = 3; i * i < n; i += 2) {
+      uint cnt = 0;
+      while (DivRem(n, i) is (var div, 0)) { n = div; cnt++; }
+      if (cnt > 0) res.Add((i, cnt));
+    }
+
+    if (n != 1) res.Add((n, 1));
+    return res;
   }
 }

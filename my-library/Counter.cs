eb5214678af49230b5ebfc483246c7f0d7c8b5f3
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-12-07 12:05:08 +0900

    Counter の実装を削減

--- my-library/Counter.cs
+++ my-library/Counter.cs
@@ -1,4 +1,5 @@
 class MultiCounter<T> : Dictionary<T, long> {
+  public bool RemoveZeros { get; init; } = true;
   [MI(256)] public MultiCounter() { }
   [MI(256)] public MultiCounter(int cap) : base(cap) { }
   [MI(256)] public MultiCounter(IEqualityComparer<T> cmp) : base(cmp) { }
@@ -9,41 +10,27 @@ class MultiCounter<T> : Dictionary<T, long> {
 
   public new long this[T k] {
     [MI(256)]
-    get { return base.TryGetValue(k, out var v) ? v : 0; }
+    get { base.TryGetValue(k, out var v); return v; }
     [MI(256)]
-    set { base[k] = value; }
-  }
-
-  [MI(256)] public void EnsureContains(T key) { if (base.ContainsKey(key)) base[key] = 0; }
-
-  [MI(256)]
-  public void Swap(T key1, T key2) {
-    var b1 = base.TryGetValue(key1, out var v1);
-    var b2 = base.TryGetValue(key2, out var v2);
-    if (b1 && b2) {
-      this[key1] = v2; this[key2] = v1;
-    } else if (b1) {
-      base.Remove(key1); this[key2] = v1;
-    } else if (b2) {
-      base.Remove(key2); this[key1] = v1;
+    set {
+      if (value == 0 && this.RemoveZeros) base.Remove(k);
+      else base[k] = value;
     }
   }
+
+  [MI(256)]
+  public void Swap(T key1, T key2)
+  => (this[key1], this[key2]) = (this[key2], this[key1]);
 
   [MI(256)]
   public bool ChMax(T key, long value) {
-    if (!base.TryGetValue(key, out var v)) {
-      this[key] = Math.Max(0, value); return value > 0;
-    }
-    if (v < value) { this[key] = value; return true; }
+    if (this[key] < value) { this[key] = value; return true; }
     return false;
   }
 
   [MI(256)]
   public bool ChMin(T key, long value) {
-    if (!base.TryGetValue(key, out var v)) {
-      this[key] = Math.Min(0, value); return value < 0;
-    }
-    if (v > value) { this[key] = value; return true; }
+    if (this[key] > value) { this[key] = value; return true; }
     return false;
   }
 }

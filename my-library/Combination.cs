dde00fd7f919a4e08237fd1ff25c0b0c02d9dc38
Roumcha <68161142+roumcha@users.noreply.github.com>
2023-12-22 18:24:50 +0900

    Combination を追加

---
+++ my-library/Combination.cs
@@ -0,0 +1,14 @@
+static class Combination {
+  [MI(256)]
+  public static T[,] PascalsTriangle<T>(int maxIncl) where T : INumberBase<T> {
+    var res = new T[maxIncl + 1, maxIncl + 1];
+    res[0, 0] = T.One;
+    for (int i = 0; i <= maxIncl; i++) {
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) res[i, j] = T.One;
+        else res[i, j] = res[i - 1, j - 1] + res[i - 1, j];
+      }
+    }
+    return res;
+  }
+}

0ab5078ce237dae3ad37d7b471220a6234b2f96c
Roumcha <68161142+roumcha@users.noreply.github.com>
2024-06-10 22:11:30 +0900

    テストを追加

---
+++ my-library-tests/_Template/TemplateTest1.cs
@@ -0,0 +1,33 @@
+namespace my_library_tests;
+
+using template;
+using Xunit.Abstractions;
+using static template.MyLib;
+
+public class TemplateTest1(ITestOutputHelper _output) {
+  [Theory]
+  [InlineData(0, 1, 0)]
+  [InlineData(1, 1, 1)]
+  [InlineData(5, 2, 10)]
+  [InlineData(61, 30, 232714176627630544)]
+  public void nCr_Valid(int n, int r, long res)
+  => Assert.Equal(res, nCr(n, r));
+
+  [Theory]
+  [InlineData(62, 30)]
+  public void nCr_Overflow(int n, int r)
+  => Assert.Throws<OverflowException>(() => nCr(n, r));
+
+  [Theory]
+  [InlineData(0, 1, 0)]
+  [InlineData(1, 1, 1)]
+  [InlineData(5, 2, 20)]
+  [InlineData(29, 14, 6761440164390912000)]
+  public void nPr_Valid(int n, int r, long res)
+    => Assert.Equal(nPr(n, r), res);
+
+  [Theory]
+  [InlineData(30, 14)]
+  public void nPr_Overflow(int n, int r)
+  => Assert.Throws<OverflowException>(() => nPr(n, r));
+}

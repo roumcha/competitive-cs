45138612738258aa26d139a8c5290e89ba9d090b
Roumcha <68161142+roumcha@users.noreply.github.com>
2024-07-26 16:54:21 +0900

    テンプレートに回文と部分列の判定を追加

---
+++ my-library-tests/TemplateTest_Seq.cs
@@ -0,0 +1,35 @@
+namespace my_library_tests;
+
+using System.Numerics;
+using FluentAssertions;
+using template;
+using Xunit.Abstractions;
+using static template.MyLib;
+
+public class TemplateTest_Seq(ITestOutputHelper _output) {
+  [Theory]
+  [InlineData("", "", true)]
+  [InlineData("", "a", true)]
+  [InlineData("a", "", false)]
+  [InlineData("a", "b", false)]
+  [InlineData("abc", "abc", true)]
+  [InlineData("ab", "abc", true)]
+  [InlineData("abc", "ab", false)]
+  [InlineData("abc", "acb", false)]
+  [InlineData("abc", "abdc", true)]
+  public static void IsSubsequenceOf_Vaild(string str, string sub, bool expected) {
+    str.IsSubsequenceOf(sub).Should().Be(expected);
+  }
+
+  [Theory]
+  [InlineData("", true)]
+  [InlineData("A", true)]
+  [InlineData("AA", true)]
+  [InlineData("Aa", false)]
+  [InlineData("AAA", true)]
+  [InlineData("AaA", true)]
+  [InlineData("Aaa", false)]
+  public static void IsPalindrome_Valid(string input, bool expected) {
+    IsPalindrome(input.AsSpan()).Should().Be(expected);
+  }
+}
--- my-library/Seq.cs
+++ my-library/Seq.cs
@@ -1,9 +1,8 @@
 public static class Seq {
   /// <remarks>O(L) where L is src's Length</remarks>
   [MI(R256)]
-  public static IEnumerable<(T Item, int Cnt)> RunLengthEncode<T>(
-    IEnumerable<T> src, T defval)
-    where T : IEquatable<T> {
+  public static IEnumerable<(T Item, int Cnt)> RunLengthEncode<T>(IEnumerable<T> src, T defval)
+  where T : IEquatable<T> {
     int c = 0; T x = defval;
     foreach (var a in src) {
       if (x.Equals(a)) { ++c; continue; }
@@ -15,8 +14,7 @@ public static class Seq {
 
   /// <remarks>O(L log L) where L is src's Length</remarks>
   [MI(R256)]
-  public static bool TryFirstDuplicate<T>(
-    IEnumerable<T> src, T defval, out int pos, out T res) {
+  public static bool TryFirstDuplicate<T>(IEnumerable<T> src, T defval, out int pos, out T res) {
     int i = 0; pos = -1; res = defval;
     var s = new HashSet<T>();
     foreach (var x in src) {
@@ -28,8 +26,7 @@ public static class Seq {
 
   /// <remarks>O(L) where L is src's Length</remarks>
   [MI(R256)]
-  public static IEnumerable<TRes> Scan<TSrc, TRes>(
-    IEnumerable<TSrc> src, TRes state, Func<TRes, TSrc, TRes> folder) {
+  public static IEnumerable<R> Scan<S, R>(IEnumerable<S> src, R state, Func<R, S, R> folder) {
     yield return state;
     foreach (var a in src) { state = folder(state, a); yield return state; }
   }
@@ -44,9 +41,8 @@ public static class Seq {
 
   /// <remarks>O(L log L) where L is src's Length</remarks>
   [MI(R256)]
-  public static Dictionary<TVal, int> CountBy<TKey, TVal>(
-    IEnumerable<TKey> src, Func<TKey, TVal> projection) {
-    var d = new Dictionary<TVal, int>();
+  public static Dictionary<V, int> CountBy<K, V>(IEnumerable<K> src, Func<K, V> projection) {
+    var d = new Dictionary<V, int>();
     foreach (var x in src) {
       var k = projection(x);
       if (d.TryGetValue(k, out var c)) d[k]++; else d[k] = 1;
